2bebaaf8716d96a11f84b5b30ab2916eb0c742b8
AmirKhamitov <124076680+AmirKhamitov@users.noreply.github.com>
2023-01-31 21:08:58 +0300

    5501 (#1)
    * sdasdasdasad
    
    * adssaddsa
    
    git ignore)

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def sayHi():
-    print("Hi")
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print('Hi');
+
+
